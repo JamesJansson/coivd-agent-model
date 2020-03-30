8fbffca95e8781a60ed25116c6f7a55ac0a13748
James Jansson <jamesjansson@gmail.com>
2020-03-31 02:48:50 +1100

    Clean up

--- main.js
+++ main.js
@@ -28,12 +28,6 @@ async function init() {
   const worker = new Worker("./worker.js");
   // WebWorkers use `postMessage` and therefore work with Comlink.
   const obj = Comlink.wrap(worker);
-  console.log(`Counter: ${await obj.counter}`);
-
-  obj.sendResult(Comlink.proxy(callback));
-
-  await obj.inc();
-  console.log(`Counter: ${await obj.counter}`);
 
   obj.runSimpleModelWrapper().then(results => {
     addToTable(results, "simple-agent-model-table");
--- worker.js
+++ worker.js
@@ -2,21 +2,6 @@ importScripts("https://unpkg.com/comlink/dist/umd/comlink.js");
 importScripts("simple-agent-model.js");
 
 const obj = {
-  counter: 0,
-  inc() {
-    this.counter++;
-    console.log("Hello from inc()");
-  },
-  async sendResult(cb) {
-    cb("callback message 1");
-    let a;
-    // Do some modelling
-    for (let i = 0; i < 1000; i++) {
-      a++;
-    }
-    console.log("console.log message");
-    cb("callback message 2");
-  },
   runSimpleModelWrapper() {
     return runSimpleAgentModel();
   }
